9ca3bba51daca12b2e079a73ae51e60ea76de637
Dinarte Jesus <dgj@nubolab.org>
2015-08-16 05:04:23 +0200

    add vendors,  css-loaders and  deploy mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,53 @@
+'use strict';
+
 var path = require('path');
+var webpack = require('webpack');
+var bowerDir = path.join(__dirname, '/bower_components');
 
-module.exports = {
-  context: path.join(__dirname, 'src'),
+var config = {
+  addVendor: function (name, src) {
+    this.resolve.alias[name] = src;
+    this.module.noParse.push(new RegExp('^' + name + '$'));
+  },
+  context: path.join(__dirname, '/src'),
   entry: {
-    javascript: './app/app.jsx',
-    html: './index.html'
+    javascript: ['webpack/hot/dev-server', './app/app.jsx'],
+    html: './index.html',
+    vendors: ['react', 'material', 'material.css']
   },
-
+  resolve: {
+    alias: {}
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'js/vendors.js')
+  ],
   output: {
-    filename: 'js/bundle.js',
-    path: path.join(__dirname, 'dist')
+    path: process.env.NODE_ENV === 'production' ? './dist' : './build',
+    filename: 'js/bundle.js'
   },
-
   module: {
+    noParse: [],
     loaders: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel-loader']
+        loader: 'babel-loader'
       },
       {
         test: /\.html$/,
+        exclude: /node_modules/,
         loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
       }
     ]
   }
 };
+config.addVendor('react', bowerDir + '/react/react.min.js');
+config.addVendor('material', bowerDir + '/material-design-lite/material.min.js');
+config.addVendor('material.css', bowerDir + '/material-design-lite/material.min.css');
+
+
+module.exports = config;
