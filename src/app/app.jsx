05932a871eaf0977d4945cf8486bcc73d700da8f
Dinarte Jesus <dgj@nubolab.org>
2015-08-28 22:34:17 +0200

    add background-image and remove margin/padding

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -2,18 +2,37 @@ import 'material.css';
 import 'material';
 
 import React from 'react';
+import Daily from './components/daily/daily.jsx';
+import Radium from 'radium';
+
 
 class App extends React.Component {
-  render () {
-    return (
-      <div class="content">
-        <h2>Hello World</h2>
-          <button className="mdl-button mdl-js-button mdl-button--raised">
-            Button
-          </button>
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div className="mdl-layout mdl-js-layout mdl-grid" style={styles}>
+                <Daily
+                    location="Berlin, Germany"
+                    lastCheck="20:00"
+                    degrees="22"
+                    icon="800"
+                    humidity="60"
+                    wind="4"
+                    pressure="1212"
+                />
+            </div>
+        );
+    }
 }
+
+var styles = {
+    width: '100%',
+    height: window.innerHeight,
+    backgroundImage: 'url(http://www.zastavki.com/pictures/originals/2014/World___Germany_View_of_the_TV_tower_from_the_river_in_Berlin_058498_.jpg)',
+    backgroundSize: 'cover',
+    margin: '0',
+    padding: '0'
+}
+
+App = Radium(App);
 
 React.render(<App/>, document.body);
