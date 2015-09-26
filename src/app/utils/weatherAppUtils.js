ce60cc02101ad722414f0feb1d8ff9f6b602aac9
Dinarte Jesus <dgj@nubolab.org>
2015-09-26 17:30:35 +0200

    example of an API for testing the app

---
+++ src/app/utils/weatherAppUtils.js
@@ -0,0 +1,29 @@
+
+var sampleWeatherAPI = [
+    {
+        location: 'London, UK',
+        degrees: 17,
+        icon: '800',
+        humidity:  60,
+        wind: 4,
+        pressure: 1220
+    },
+    {
+        location: 'Paris, FR',
+        degrees: 12,
+        icon: '800',
+        humidity:  60,
+        wind: 4,
+        pressure: 1220
+    },
+    {
+        location: 'Berlin, GR',
+        degrees: 15,
+        icon: '800',
+        humidity:  60,
+        wind: 4,
+        pressure: 1220
+    }
+];
+
+module.exports = sampleWeatherAPI;
