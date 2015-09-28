f0919529a5f28657287e108cf22be2a39ce88762
Dinarte Jesus <dgj@nubolab.org>
2015-09-28 13:42:04 +0200

    add API call and store conections

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -2,49 +2,54 @@ import 'material.css';
 import 'material';
 
 import React from 'react';
-import Radium from 'radium';
+
 import Daily from './components/daily/daily.jsx';
+import Background from './components/background/background.jsx';
 import DailyStore from './components/daily/dailyStore';
+
+import WeatherData from './utils/DailyWeatherData';
+import DailyAPI from './utils/DailyAPI';
+
+WeatherData.init();
+
+DailyAPI.getWeatherData();
+
+function getWeatherState() {
+    return {
+        weather: DailyStore.getDailyWeather(),
+    };
+};
 
 var App = React.createClass({
     getInitialState() {
-        return {
-        };
+        return getWeatherState();
     },
-    componentWillMount() {
-        console.log('Hello Word');
+    componentDidMount: function() {
+        DailyStore.addChangeListener(this._onChange);
     },
-    componentDidMount() {
-        console.log('Good Bye');
+    componentWillUnmount: function() {
+        DailyStore.removeChangeListener(this._onChange);
+    },
+    _onChange: function() {
+        this.setState(getWeatherState());
     },
     render () {
         return (
-            <div className="mdl-layout mdl-js-layout mdl-grid" style={styles}>
+            <Background>
                 <section className="section--center mdl-grid mdl-cell--middle">
                     <Daily
-                        location="Berlin, Germany"
-                        lastCheck="20:00"
-                        degrees="22"
-                        icon="800"
-                        humidity="60"
-                        wind="4"
-                        pressure="1212"
+                        location={this.state.weather.location}
+                        lastCheck="19:00"
+                        degrees={this.state.weather.degrees}
+                        icon={this.state.weather.icon}
+                        humidity={this.state.weather.humidity}
+                        wind={this.state.weather.wind}
+                        pressure={this.state.weather.pressure}
                     />
                 </section>
-            </div>
+            </Background>
         );
     }
 });
-
-var styles = {
-    width: '100%',
-    height: window.innerHeight,
-    backgroundImage: 'url(http://www.zastavki.com/pictures/originals/2014/World___Germany_View_of_the_TV_tower_from_the_river_in_Berlin_058498_.jpg)',
-    backgroundSize: 'cover',
-    margin: '0',
-    padding: '0'
-};
-
-App = Radium(App);
 
 React.render(<App/>, document.body);
