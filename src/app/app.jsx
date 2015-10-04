8c080e0f9232ce3419764421348214a4b8ac6f6f
Dinarte Jesus <dgj@nubolab.org>
2015-10-04 13:55:49 +0200

    remove unused example Api, change functions to es6 and connect new API

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -5,14 +5,10 @@ import React from 'react';
 
 import Daily from './components/daily/daily.jsx';
 import Background from './components/background/background.jsx';
+import Locations from './components/locations/locations.jsx';
+
 import DailyStore from './components/daily/dailyStore';
-
-import WeatherData from './utils/DailyWeatherData';
-import DailyAPI from './utils/DailyAPI';
-
-WeatherData.init();
-
-DailyAPI.getWeatherData();
+import DailyActions from './components/daily/dailyActions';
 
 function getWeatherState() {
     return {
@@ -24,29 +20,30 @@ var App = React.createClass({
     getInitialState() {
         return getWeatherState();
     },
-    componentDidMount: function() {
+    componentDidMount () {
+        DailyActions.receiveData('Berlin, de');
         DailyStore.addChangeListener(this._onChange);
     },
-    componentWillUnmount: function() {
+    componentWillUnmount () {
         DailyStore.removeChangeListener(this._onChange);
     },
-    _onChange: function() {
+    _onChange () {
         this.setState(getWeatherState());
     },
     render () {
         return (
             <Background>
-                <section className="section--center mdl-grid mdl-cell--middle">
+                <Locations>
                     <Daily
                         location={this.state.weather.location}
-                        lastCheck="19:00"
+                        lastCheck={this.state.weather.lastCheck}
                         degrees={this.state.weather.degrees}
                         icon={this.state.weather.icon}
                         humidity={this.state.weather.humidity}
                         wind={this.state.weather.wind}
                         pressure={this.state.weather.pressure}
                     />
-                </section>
+                </Locations>
             </Background>
         );
     }
