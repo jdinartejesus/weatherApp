f61e4f8220c40ba4e663e0384fa60e862c8b8489
Dinarte Jesus <dgj@nubolab.org>
2015-10-04 13:56:22 +0200

    remove example api

--- src/app/utils/DailyAPI.js
+++
@@ -1,8 +0,0 @@
-import DailyActions from '../components/daily/dailyActions';
-
-module.exports = {
-    getWeatherData () {
-        var data = JSON.parse(localStorage.getItem('weather'));
-        DailyActions.receiveData(data);
-    }
-};
