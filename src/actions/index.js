22183ac4c70684293cf2d5817000c5c48fc3747e
Dinarte Jesus <jdinartejesus@gmail.com>
2015-12-05 23:57:31 +0100

    Add fetching to the API and related actions
    Note: Sill need to be organize and refactor

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,63 @@
+import request from 'axios';
+import fetch from 'isomorphic-fetch'
+
 /*
  * action types
  */
 
-export const UPDATE = 'UPDATE'
-export const NEXT_LOCATION = 'NEXT_LOCATION'
-export const PREVIOUS_LOCATION = 'PREVIOUS_LOCATION'
+export const REQUEST_DATA = 'REQUEST_DATA'
+export const RECEIVE_DATA = 'RECEIVE_DATA'
+export const INVALIDE_DATA = 'INVALIDE_DATA'
 
 /*
-* action creators
-*/
+ * action creators
+ */
 
-export function update() {
-  return { type: UPDATE }
+const API_URL = 'http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2de143494c0b295cca9337e1e96b00e0'
+
+export function requestWeather(location) {
+    return {
+        type: REQUEST_DATA,
+        location
+    }
 }
 
-export function nextLocation() {
-  return { type: NEXT_LOCATION }
+export function invalideWeather(err, msg) {
+    return {
+        type: INVALIDE_DATA,
+        error: err,
+        message: msg
+    }
 }
 
-export function previousLocation() {
-  return { type: PREVIOUS_LOCATION }
+export function receiveWeather(json) {
+    return {
+        type: RECEIVE_DATA,
+        daily: json,
+        receivedAt: Date.now()
+    }
+}
+
+export function fetchLocations(location) {
+    return dispatch => {
+        dispatch(requestWeather(location))
+        return fetch(API_URL)
+            .then(req => req.json())
+            .then(fetchStatus)
+            .then(function(data) {
+                dispatch(receiveWeather(data))
+            }).catch(function(error) {
+                dispatch(invalideWeather(error.response.cod, error.message))
+            })
+    }
+}
+
+function fetchStatus(response){
+    if(response.cod >= 200 && response.cod < 300){
+        return response
+    }else{
+        var error = new Error(response.message)
+        error.response = response
+        throw error
+    }
 }
