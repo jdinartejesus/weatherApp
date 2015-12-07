e02b6f7b5d46afd20e09f794fb10b74c0ec58c8a
Dinarte Jesus <jdinartejesus@gmail.com>
2015-12-07 01:17:51 +0100

    adapted the reducers to the API calls

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,38 @@
-import {UPDATE, NEXT_LOCATION, PREVIOUS_LOCATION } from '../actions'
+import { combineReducers } from 'redux'
+import {REQUEST_DATA, RECEIVE_DATA, INVALIDE_DATA} from '../actions'
 
-const initialState = [
-    {
-        id: 802,
-        main: "Clouds",
-        description: "scattered clouds",
-        icon: "03n"
-    }
-]
+//TODO: Immutable
+const initialState = {
+    isFetching: false,
+    isError: false,
+    isLoaded: false,
+    weather: {}
+}
 
 export default function weatherApp(state = initialState, action) {
     switch (action.type) {
-        case UPDATE:
-            return state
-        case NEXT_LOCATION:
-            return state
-        case PREVIOUS_LOCATION:
-            return state
+        case REQUEST_DATA:
+            return Object.assign({}, state, {
+                isFetching: true,
+                isError: false,
+                isLoaded: false
+            });
+        case RECEIVE_DATA:
+            return Object.assign({}, state, {
+                isFetching: false,
+                isError: false,
+                isLoaded: true,
+                weather: action.daily,
+                lastUpdated: action.receivedAt
+            })
+        case INVALIDE_DATA:
+            return Object.assign({}, state, {
+                isFetching: false,
+                isError: true,
+                isLoaded: true,
+                error: action.error,
+                message: action.message
+            });
         default:
             return state
     }
