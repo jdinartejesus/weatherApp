04deb7ccd6ee567bc566398da3bd36a84dedc90c
Dinarte Jesus <jdinartejesus@gmail.com>
2015-12-07 01:15:02 +0100

    add middleware and react-thunk
    Please note this file gonna be refactor and move the middleware to the store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/'
 import weatherApp from './reducers/'
 
-let store = createStore(weatherApp);
+const loggerMiddleware = createLogger()
+const createStoreWithMiddleware = applyMiddleware(
+  thunkMiddleware, // lets us dispatch() functions
+  loggerMiddleware // neat middleware that logs actions
+)(createStore)
+
+let store = createStoreWithMiddleware(weatherApp);
+
 
 ReactDOM.render(
   <Provider store={store}>
