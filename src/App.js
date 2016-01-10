438767dff080abe02344cbd72837fade3067ddc8
Dinarte Jesus <jdinartejesus@gmail.com>
2016-01-10 19:47:35 +0100

    Moved function configstore to store folder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import thunkMiddleware from 'redux-thunk'
-import createLogger from 'redux-logger'
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/'
-import weatherApp from './reducers/'
+import configureStore from './store/'
 
-const loggerMiddleware = createLogger()
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware, // lets us dispatch() functions
-  loggerMiddleware // neat middleware that logs actions
-)(createStore)
-
-let store = createStoreWithMiddleware(weatherApp);
-
+let store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import weatherApp from '../reducers/'
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+
+const loggerMiddleware = createLogger()
+
+const createStoreWithMiddleware = applyMiddleware(
+  thunkMiddleware, // lets us dispatch() functions
+  loggerMiddleware // neat middleware that logs actions
+)(createStore)
+
+export default function configureStore(initialState) {
+  return createStoreWithMiddleware(weatherApp, initialState)
+}
