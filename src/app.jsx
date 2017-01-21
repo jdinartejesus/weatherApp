8a0d827121c5a18c2ba4077fdbdb8e8afb1fa7fc
Dinarte Jesus <jdinartejesus@gmail.com>
2017-01-21 13:54:29 +0100

    Refactor webpack and removed bower dependency

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,11 @@ import { Provider } from 'react-redux'
 import App from './containers/'
 import configureStore from './store/'
 
-let store = configureStore();
+let store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
-);
+  document.getElementById('app')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,47 @@
-'use strict';
+'use strict'
 
-var path = require('path');
-var webpack = require('webpack');
-var bowerDir = path.join(__dirname, '/bower_components');
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-var config = {
-    addVendor: function(name, src) {
-        this.resolve.alias[name] = src;
-        this.module.noParse.push(new RegExp('^' + name + '$'));
-    },
-    context: path.join(__dirname, '/src'),
-    entry: {
-        javascript: ['webpack/hot/dev-server', './App.js'],
-        html: './index.html',
-        vendors: ['material', 'material.css', 'weather.icons']
-    },
-    resolve: {
-        alias: {}
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin('vendors', 'js/vendors.js')
-    ],
-    output: {
-        path: process.env.NODE_ENV === 'production' ? './dist' : './build',
-        filename: 'js/bundle.js'
-    },
-    module: {
-        noParse: [],
-        loaders: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }, {
-            test: /\.html$/,
-            exclude: /node_modules/,
-            loader: 'file?name=[name].[ext]'
-        }, {
-            test: /\.css$/,
-            loader: 'style-loader!css-loader'
-        }, {
-            test: /\.scss$/,
-            loaders: ["style", "css", "sass"]
-        }, {
-            test: /\.(woff|ttf|woff(2)?)$/,
-            loader: 'url-loader'
-        }, {
-            test: /\.(eot|svg)$/,
-            loader: 'file-loader'
-        }]
-    }
-};
-
-config.addVendor('material', bowerDir + '/material-design-lite/material.min.js');
-config.addVendor('material.css', bowerDir + '/material-design-lite/material.min.css');
-config.addVendor('weather.icons', bowerDir + '/weather-icons/css/weather-icons.min.css');
-
-module.exports = config;
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: 'app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'assets/js/bundle.js'
+  },
+  resolve: {
+    modules: [path.join(__dirname, '/src'), 'node_modules'],
+    extensions: ['.js', '.json', '.jsx', '.scss']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('assets/styles/style.css'),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+      files: {
+        css: ['assets/style/style.css']
+      }
+    })
+  ]
+}
